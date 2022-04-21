6f117693e08387e25a8a5b03958f02b1de3da67e
tauhid <tauhid495@gmail.com>
2022-04-21 22:00:40 +0600

    ho\

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const { listen } = require('express/lib/application');
-const app= express();
-const port=process.env.PORT||5000;
+const app = express();
+const port = process.env.PORT || 5000;
+
+// mongodb user
+// name: user1db
+// password: tauhid.1691
+
+
+
+const uri = "mongodb+srv://user1db:tauhid.1691@cluster0.3nbaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('juceExpress').collection('user');
+
+        app.post('/user', (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            res.send('user date received.')
+
+
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
- res.send('Node mongo db')
+app.get('/', (req, res) => {
+    res.send('Node mongo db is running')
 });
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Crud is running');
 })
